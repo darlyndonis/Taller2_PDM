91782ff9e1c49ba49c8dc0c41a7c154679c7b1e6
darlyndonis <00083823@uca.edu.sv>
2025-04-29 22:58:43 -0600

    Agregando cambios finales

--- app/src/main/java/com/pdmtaller2/DarlynDonis_00083823/data/dummy/DummyMovies.kt
+++ app/src/main/java/com/pdmtaller2/DarlynDonis_00083823/data/dummy/DummyMovies.kt
@@ -6,110 +6,110 @@ import com.pdmtaller2.DarlynDonis_00083823.data.model.MenuItem
 val dummyRestaurants = listOf(
     Restaurant(
         id = 1,
-        name = "Taco Loco",
+        name = "TacoBell",
         description = "Tacos auténticos mexicanos con ingredientes frescos.",
         category = "Comida Mexicana",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://brandemia.org/sites/default/files/inline/images/logo_taco_bell_antes_0.jpg",
+        imageUrl = "https://brandemia.org/sites/default/files/inline/images/logo_taco_bell_antes_0.jpg",
         menu = listOf(
-            MenuItem(1, "Taco de Carne Asada", "Con guacamole y salsa roja", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(2, "Burrito de Pollo", "Envueltos en tortilla grande con arroz", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(1, "Taco de pollo", "Con guacamole y salsa roja", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(2, "Burrito de carne de res", "Envueltos en tortilla grande con arroz", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     ),
     Restaurant(
         id = 2,
-        name = "Pasta Bella",
+        name = "Food Italian",
         description = "Especialistas en cocina italiana tradicional.",
         category = "Comida Italiana",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://img.freepik.com/vector-premium/comida-italiana-logo-nombre-icono-simbolo-vector-italia_526280-402.jpg",
+        imageUrl = "https://img.freepik.com/vector-premium/comida-italiana-logo-nombre-icono-simbolo-vector-italia_526280-402.jpg",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     ),
     Restaurant(
         id = 3,
-        name = "Pasta no tan Bella",
+        name = "Monterosso Trattoria",
         description = "Especialistas en cocina italiana.",
         category = "Comida Italiana",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTawWeiCOXJTZYWF1DYPNPKzVDBj6vqCTU7nVmbcatQvQMp2gZlgFJNLaSeW0UGI2I374&usqp=CAU",
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTawWeiCOXJTZYWF1DYPNPKzVDBj6vqCTU7nVmbcatQvQMp2gZlgFJNLaSeW0UGI2I374&usqp=CAU",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "Mozzarella en capresse ", "Pan de carne, queso y tomate", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Ensalada Capresse", "Capas de ensalada fresca, carne y queso", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     ),
     Restaurant(
         id = 4,
-        name = "Burguer Queen",
+        name = "Burguer King",
         description = "Especialistas en Hamburguesas.",
         category = "Comida Rapida",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHcjv0wmeIzNl0MAwpCZ77X3qzKnyuufZZ0w&s",
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHcjv0wmeIzNl0MAwpCZ77X3qzKnyuufZZ0w&s",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "Combo king de pollo", "Pollo asado con pan de masa madre", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Hamburguesa big king", "Capas de carne, papas y queso", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     ),
     Restaurant(
         id = 5,
-        name = "Sushi Loco",
+        name = "Sushiito",
         description = "Especialistas en cocina asiatica.",
         category = "Comida Asiatica",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://funocomercial.com/pabellon-salina-cruz/wp-content/uploads/sites/68/2025/01/Diseno-sin-titulo-62.png",
+        imageUrl = "https://funocomercial.com/pabellon-salina-cruz/wp-content/uploads/sites/68/2025/01/Diseno-sin-titulo-62.png",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "Camarones Yuzzu", "Camarones al tempura, aderezados con salsa especial de limón; servidos con un toque de cebollín y salsa picante", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Yakitori", "Brocheta asada y marinada con salsa Teriyaki", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
         )
     ),
     Restaurant(
         id = 6,
-        name = "Otaku Ramen",
+        name = "PandaExpress",
         description = "Especialistas en cocina asiatica.",
         category = "Comida Asiatica",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://play-lh.googleusercontent.com/yfXETQ7tmoxN9vzZegwQMosHZuRvVYRSuhFV4aV23j6vwg_9s8BPfNFqfuQpfrvB4Uk",
+        imageUrl = "https://play-lh.googleusercontent.com/yfXETQ7tmoxN9vzZegwQMosHZuRvVYRSuhFV4aV23j6vwg_9s8BPfNFqfuQpfrvB4Uk",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "tacos asiaticos", "Deliciosos tacos de Asia endulzados", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Ramen asiatico", "Delicioso plato de ramen con ingredientes frescos", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     ),
     Restaurant(
         id = 7,
-        name = "Go Verde",
+        name = "El Veggie SV",
         description = "Especialistas en cocina saludable.",
         category = "Comida Saludable",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvZHgX5-RSTq9gGyz8bReVDD2SwbeFb9eXw&s",
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFvZHgX5-RSTq9gGyz8bReVDD2SwbeFb9eXw&s",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "Quinoa falafel Bowl", "Deliciosa quinoa hecho de la casa", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Burguer de la casa", "Deliciosas hamburguesa con ingredientes de origen vegetal", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     ),
     Restaurant(
         id = 8,
-        name = "Sweets & Treats",
+        name = "Delikat",
         description = "Especialistas en postres.",
         category = "Postres",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZGnN4jkcJBhQ7GhMT7aoZpeNC1Yy93SVPkg&s",
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZGnN4jkcJBhQ7GhMT7aoZpeNC1Yy93SVPkg&s",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "Mousse de chocolate blanco y café", "Masa sablé de almendra y un suave bizcocho humedecido de café", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Tartaletta de blueberry", "Masa sablé de almendra, crema de almendra con blueberris, crema pastelera con vainilla natural", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     ),
     Restaurant(
         id = 9,
-        name = "Tabernon",
+        name = "Starbucks",
         description = "Especialistas en bebidas.",
         category = "Bebidas",
-        backdropUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
-        imageUrl = "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg",
+        backdropUrl = "https://media.revistagq.com/photos/5d5d383031110c000879872d/1:1/w_1080,h_1080,c_limit/logo-starbucks.jpg",
+        imageUrl = "https://media.revistagq.com/photos/5d5d383031110c000879872d/1:1/w_1080,h_1080,c_limit/logo-starbucks.jpg",
         menu = listOf(
-            MenuItem(3, "Spaghetti Bolognese", "Salsa de carne y tomate", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg"),
-            MenuItem(4, "Lasagna Clásica", "Capas de pasta, carne y queso", "https://i.pinimg.com/736x/17/2f/cc/172fcc6c37fa371017fcd0532c60339a.jpg")
+            MenuItem(3, "Hot Coffee", "Salsa de carne y tomate", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc="),
+            MenuItem(4, "Caramel Frappuchino", "Capas de pasta, carne y queso", "https://media.istockphoto.com/id/943483254/es/vector/dulce-sabrosa-a-la-parrilla-piernas-de-pavo-de-pollo-asado-con-verduras-en-rodajas-patatas.jpg?s=612x612&w=0&k=20&c=PDQQxf_d2yEfENnlVGpOXJg0dexHd-TD8sTjagsWuDc=")
         )
     )
 )
